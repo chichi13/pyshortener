d241ae613e2a70487ff734b4d0931d36912c49e5
Antoine C <antoinechiris@gmail.com>
2022-11-13 19:18:21 +0100

    add: Subtitle

--- pyshortener-ui/src/components/ComponentHeader.vue
+++ pyshortener-ui/src/components/ComponentHeader.vue
@@ -12,7 +12,12 @@
           d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
         />
       </svg>
-      <span class="font-semibold text-xl tracking-tight">PyShortener</span>
+      <span class="font-semibold text-xl tracking-tight">PyShortener </span>
+      <span
+        class="hidden sm:flex block text-right text-gray-200 hover:text-white"
+      >
+        &nbsp;- An open source URL shortener
+      </span>
     </div>
   </nav>
 </template>
